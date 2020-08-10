0c3ba02da277c04c4c6edb8fe3a247ae38db6eed
lisa-rllr <68598265+lisa-rllr@users.noreply.github.com>
2020-08-10 10:06:15 +0200

    MAJ fonction pour obtenir la résolution

--- agriculture_precision/algorithms/functions/general.py
+++ agriculture_precision/algorithms/functions/general.py
@@ -3,17 +3,14 @@
 Created on Mon July 21 2020
 @author: Lisa R
 """
-
-def pixel_size(layer, resolution) :
-    """ permet d'obtenir la taille des pixels du raster 
+   
+def pixel_resolution(layer, pixel):
+    """ permet d'obtenir la résolution en pixels du raster 
     que l'on veut en sortie à partir de la taille du 
-    vecteur de départ et de la résolution que l'on veut"""
+    pixel que l'on veut"""
     ex = layer.extent()
     xlength = ex.xMaximum() - ex.xMinimum()
     ylength = ex.yMaximum() - ex.yMinimum()
-    if ylength>xlength :
-        pixel_size = ylength/resolution
-    else :
-        pixel_size = xlength/resolution
-        
-    return pixel_size
+    resolution_x=abs(xlength/pixel)
+    resolution_y=abs(ylength/pixel)
+    return resolution_x,resolution_y
