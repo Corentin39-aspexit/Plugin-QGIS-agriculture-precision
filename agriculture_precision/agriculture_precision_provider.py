e3cb3f9c66d25e770df99d546095abac5956fc0d
lisa-rllr <68598265+lisa-rllr@users.noreply.github.com>
2020-08-21 11:55:35 +0200

    Update agriculture_precision_provider.py

--- agriculture_precision/agriculture_precision_provider.py
+++ agriculture_precision/agriculture_precision_provider.py
@@ -41,7 +41,7 @@ from .algorithms.echantillonnage_polygone import EchantillonnagePolygone
 from .algorithms.zonage_par_classif import ZonageClassification
 from .algorithms.tesselations import Tesselations
 from .algorithms.xml_to_shp import XmlToShp
-#from .algorithms.indice_zonage import IndiceZonage
+from .algorithms.indice_zonage import IndiceZonage
 from .algorithms.filtrage_donnees_spatiales import FiltreDonneesSpatiales
 
 class AgriculturePrecisionProvider(QgsProcessingProvider):
@@ -74,7 +74,7 @@ class AgriculturePrecisionProvider(QgsProcessingProvider):
         self.addAlgorithm(FiltreDonnees())
         self.addAlgorithm(Tesselations())
         self.addAlgorithm(InterpolationPoints())
-        #self.addAlgorithm(IndiceZonage())
+        self.addAlgorithm(IndiceZonage())
         self.addAlgorithm(FiltreDonneesSpatiales())
         #Autre
         self.addAlgorithm(XmlToShp())
